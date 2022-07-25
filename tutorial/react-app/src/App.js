b1a93a1a69d3a52f8a28987d289bb245e5fd7d5b
agungsriyono <96234293+agungsriyono@users.noreply.github.com>
2022-07-25 15:32:04 +0700

    Update 20220725a

--- tutorial/react-app/src/App.js
+++ tutorial/react-app/src/App.js
@@ -1,13 +1,71 @@
-import Header from "./components/Header";
-
+// Menggunakan state
+import { useState } from "react"
 function App() {
+  const [title, setTitle] = useState("Hello World");
+  const changeTitle = () => {
+    setTitle("Title Changed");
+  }
   return (
     <div>
-      <Header/>
-      <h2> Hello dunia</h2>
-
+      <h1>{title}</h1>
+      <button onClick={changeTitle}>Change Title</button>
     </div>
   );
 }
-
 export default App;
+
+
+// // Event with value
+// import Header from "./components/Header";
+// function App() {
+//   const clickMe = (name) => {
+//     console.log('Hello: '+ name);
+//   }
+//   return (
+//     <div>
+//       <Header />
+//       <button onClick={ () => clickMe('Test kirim var') }>Click Me</button>
+//     </div>
+//   );
+// }
+// export default App;
+
+// Event doang
+// import Header from "./components/Header";
+// function App() {
+//   const clickMe = () => {
+//     console.log('Clicked');
+//   }
+//   return (
+//     <div>
+//       <Header />
+//       <button onClick={ clickMe }>Click Me</button>
+//     </div>
+//   );
+// }
+// export default App;
+
+
+
+// import Header from "./components/Header";
+// // function App() {
+// //   return (
+// //     <div>
+// //       <Header/>
+// //       <h2> Hello dunia</h2>
+// //     </div>
+// //   );
+// // }
+
+// function App() {
+//   const clickMe = () => {
+//     console("clicked Me");
+//   };
+//   return (
+//     <div>
+//       <Header />
+//       <button onClick={clickMe}>Klik Me</button>
+//     </div>
+//   )
+// }
+// export default App;
